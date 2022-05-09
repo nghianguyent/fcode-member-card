02cba7a79d31dd6cf76ef94bddb27db6f5048874
Chin Riku <54855318+khanhdinh02@users.noreply.github.com>
2022-05-09 14:39:26 +0700

    create 'events' API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const env = require('dotenv');
-const pool = require('./utilities/db');
-const Database = require('./services/database');
+const bodyParser = require('body-parser');
+const eventsRoute = require('./routes/events');
 
 const app = express();
 
@@ -9,30 +9,12 @@ const port = process.env.PORT || 3000;
 
 env.config();
 
-// test db connection
-Database.getEvents((err, res) => {
-	if (err) console.error(err);
-	else console.log(res);
-});
-// Database.addEvent('name2', '2022-12-15', null, '0:0:0', null, 'NOT YET', 'SP2022', (err, res) => {
-// 	if (err) console.error(err);
-// 	else console.log(res);
-// });
-// Database.updateEvent('1651493389268', 'nameupdate', '2022-12-15', null, '0:0:0', null, 'NOT YET', 'SP2022', (err, res) => {
-// 	if (err) console.error(err);
-// 	else console.log(res);
-// });
-// Database.deleteEvent('1651493389268', (err, res) => {
-// 	if (err) console.error(err);
-// 	else console.log(res);
-// });
-
-// routes
-app.get('/', (req, res) => {
-	console.log('Hello, world!');
-	res.status(200).send('hello, world!');
-});
-
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routes
+eventsRoute(app);
---
+++ models/event.js
@@ -0,0 +1,82 @@
+const pool = require('../utilities/db').getPool();
+
+class Event {
+	constructor(event) {
+		this.id = Date.now();
+		this.name = event.name;
+		this.date = event.date;
+		this.description = event.description;
+		this.startTime = event.start_time;
+		this.endTime = event.end_time;
+		this.status = event.status;
+		this.semester = event.semester;
+	}
+
+	static getAll = (result) => {
+		pool.query('SELECT * FROM event ORDER BY id DESC', (err, res) => {
+			if (err) result(err, null);
+			else result(null, res);
+		});
+	};
+
+	static add = (event, result) => {
+		const query = 'INSERT INTO event VALUES(?,?,?,?,?,?,?,?)';
+		pool.query(
+			query,
+			[
+				event.id,
+				event.name,
+				event.date,
+				event.description,
+				event.startTime,
+				event.endTime,
+				event.status,
+				event.semester,
+			],
+			(err, res) => {
+				if (err) result(err, null);
+				else result(null, res);
+			}
+		);
+	};
+
+	static get = (id, result) => {
+		pool.query('SELECT * FROM event WHERE id=?', [id], (err, res) => {
+			if (err) result(err, null);
+			else result(null, res);
+		});
+	};
+
+	static set = (id, event, result) => {
+		const query =
+			'UPDATE event SET name=?, date=?, description=?,' +
+			'start_time=?, end_time=?, status=?, semester=?' +
+			'WHERE id=?';
+		pool.query(
+			query,
+			[
+				event.name,
+				event.date,
+				event.description,
+				event.startTime,
+				event.endTime,
+				event.status,
+				event.semester,
+				id,
+			],
+			(err, res) => {
+				if (err) result(err, null);
+				else result(null, res);
+			}
+		);
+	};
+
+	static remove = (id, result) => {
+		pool.query('DELETE FROM event WHERE id=?', [id], (err, res) => {
+			if (err) result(err, null);
+			else result(null, res);
+		});
+	};
+}
+
+module.exports = Event;
