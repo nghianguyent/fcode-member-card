4c46bf2d9b97c79ee5b5b5b9cdf55f98c27154db
nghia <nghia14302@gmail.com>
2022-05-24 15:54:35 +0700

    Create get user data

---
+++ controllers/memberController.js
@@ -0,0 +1,33 @@
+const Member = require('./../models/member');
+const jwt = require('./../utilities/jwt');
+const config = require('./../configs');
+
+const getUserById = (req, res) => {
+	jwt.verifyToken(req.headers.token, config.JWT_SECRET)
+		.then(() => {
+			Member.getUserById(req.params.id, (err, result) => {
+				if (err) return res.status(500).send(err.message);
+				if (!result)
+					return res.status(200).json({
+						status: 403,
+						message: 'Could not find user with id ' + req.params.id,
+					});
+				res.status(200).json({
+					status: 200,
+					message: 'Successfully get user ' + req.params.id,
+					data: result,
+				});
+			});
+		})
+		.catch((err) => {
+			res.status(200).json({
+				status: 403,
+				message: 'False to get data, ' + err.message,
+			});
+			console.log(err.message);
+		});
+};
+
+module.exports = {
+	getUserById: getUserById,
+};
--- queries/queryModal.js
+++ queries/queryModal.js
@@ -1,3 +1,4 @@
 module.exports = {
 	getUserByEmail: 'SELECT * FROM member WHERE school_mail = ? or personal_mail= ?',
+	getUserById: 'SELECT * FROM member WHERE id = ?',
 };
