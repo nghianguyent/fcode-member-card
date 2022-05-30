9ffc7b0d0667f16d8b73351f44291c840e842fb1
nghia <nghia14302@gmail.com>
2022-05-30 11:16:58 +0700

    update end_date attribute to event

--- models/event.js
+++ models/event.js
@@ -4,7 +4,7 @@ class Event {
 	constructor(event) {
 		this.id = Date.now();
 		this.name = event.name;
-		this.eventDate = event.event_date;
+		this.startDate = event.start_date;
 		this.description = event.description;
 		this.startTime = event.start_time;
 		this.endTime = event.end_time;
@@ -14,19 +14,20 @@ class Event {
 
 	static getAll = (result) => {
 		pool.query('SELECT * FROM event ORDER BY id DESC', (err, res) => {
-			if (err) result(err, null);
-			else result(null, res);
+			if (err) return result(err, null);
+			return result(null, res);
 		});
 	};
 
 	static add = (event, result) => {
-		const query = 'INSERT INTO event VALUES(?,?,?,?,?,?,?,?)';
+		const query = 'INSERT INTO event VALUES(?,?,?,?,?,?,?,?,?)';
 		pool.query(
 			query,
 			[
 				event.id,
 				event.name,
-				event.eventDate,
+				event.startDate,
+				event.endDate,
 				event.description,
 				event.startTime,
 				event.endTime,
@@ -42,21 +43,22 @@ class Event {
 
 	static get = (id, result) => {
 		pool.query('SELECT * FROM event WHERE id=?', [id], (err, res) => {
-			if (err) result(err, null);
-			else result(null, res);
+			if (err) return result(err, null);
+			return result(null, res);
 		});
 	};
 
 	static set = (id, event, result) => {
 		const query =
-			'UPDATE event SET name=?, date=?, description=?, ' +
+			'UPDATE event SET name=?, start_date=?, end_date=?, description=?, ' +
 			'start_time=?, end_time=?, status=?, semester=? ' +
 			'WHERE id=?';
 		pool.query(
 			query,
 			[
 				event.name,
-				event.eventDate,
+				event.startDate,
+				event.endDate,
 				event.description,
 				event.startTime,
 				event.endTime,
@@ -73,8 +75,8 @@ class Event {
 
 	static remove = (id, result) => {
 		pool.query('DELETE FROM event WHERE id=?', [id], (err, res) => {
-			if (err) result(err, null);
-			else result(null, res);
+			if (err) return result(err, null);
+			else return result(null, res);
 		});
 	};
 }
